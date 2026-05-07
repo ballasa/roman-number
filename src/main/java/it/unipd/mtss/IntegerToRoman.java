acf0f4fca7863793f4c12b5c7da9fc35b9c092e8
Davide <davideloparco.03@gmail.com>
2026-05-07 09:38:25 +0200

    Fix della formattazione per rispettare le regole di checkstyle.

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -18,7 +18,8 @@ public class IntegerToRoman {
     public static String convert(int number){
         //Corrispondenza tra numeri romani e arabi (simboli singoli e casi sottrattivi);
         int[] values={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romanSymbols={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] romanSymbols={"M", "CM", "D", "CD", "C", "XC",
+         "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder result = new StringBuilder();
 
